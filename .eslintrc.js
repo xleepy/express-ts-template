78330f1565646a2255555e3c349f94f95ce7e5dc
Radmir Gurkov <ragux0@gmail.com>
2024-01-27 18:46:49 +0100

    add prettier eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'plugin:security/recommended-legacy'],
+  extends: [
+    'standard-with-typescript',
+    'plugin:security/recommended-legacy',
+    'plugin:prettier/recommended',
+  ],
   overrides: [
     {
       env: {
