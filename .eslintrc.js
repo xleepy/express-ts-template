7fcfe911b7d021988479435fc22b561d131a9467
Radmir Gurkov <ragux0@gmail.com>
2024-01-27 16:22:20 +0100

    Add json example

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'plugin:security/recommended'],
+  extends: ['standard-with-typescript'],
   overrides: [
     {
       env: {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import fs from 'fs';
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.use(helmet());
 
 app.get('/', (_, res) => {
   res.send('hello world');
+});
+
+app.post('/test', (req, res) => {
+  fs.writeFileSync('./test.json', JSON.stringify(req.body));
+  res.send(200);
 });
 
 app.listen(3000, () => {
