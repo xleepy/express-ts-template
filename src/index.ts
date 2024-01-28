214d77b7a3ff6832de493584c9efbd2172361426
Radmir Gurkov <ragux0@gmail.com>
2024-01-28 14:13:03 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import fs from 'fs';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
